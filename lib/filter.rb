5c3d7e91025bcb52c6db62914d8f2b6d05a8d96c
patricia <prmonday@gmail.com>
2021-02-27 07:06:40 +0000

    check highest value method

--- lib/filter.rb
+++ lib/filter.rb
@@ -2,4 +2,8 @@ class Filter
   def check_lower_value(input_value)
     40
   end
+
+  def check_highest_value(input_value)
+    1000
+  end
 end
--- spec/filter_spec.rb
+++ spec/filter_spec.rb
@@ -6,4 +6,9 @@ filter = Filter.new
 filter.check_lower_value(10)
 expect(filter.check_lower_value(10)).to eq(40)
     end
+    it 'checks if the input frequency is the same as the default hiehest value'do
+      filter = Filter.new
+      filter.check_highest_value(1500)
+      expect(filter.check_highest_value(1500)).to eq(1000)
+    end
 end
