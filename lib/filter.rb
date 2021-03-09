c2c58a3b296c5b15ff2354135a626aaa10cb7190
patricia <prmonday@gmail.com>
2021-03-09 09:58:36 +0000

    check_lower_value method mpassed test

--- lib/filter.rb
+++ lib/filter.rb
@@ -1,9 +1,17 @@
 class Filter
-  def check_lower_value(input_value)
-    40
-  end
 
-  def check_highest_value(input_value)
-    1000
+
+  def check_lower_value(input_value)
+    output_values = []
+    input_value.each do |element|
+    if element <= 40
+      output_values.push(40)
+    elsif element > 40
+      output_values.push(element)
+
+    end
+
+  end
+    return output_values
   end
 end
